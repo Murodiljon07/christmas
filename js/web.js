7bacdb3d939bd66a5a1d53c8569c02b1549641e7
Murodiljon07 <murodiljonabdumutalovtg1@gmail.com>
2025-12-02 19:43:52 +0500

    upgaradet and add transition

--- js/web.js
+++ js/web.js
@@ -1,7 +1,30 @@
-const ModeBtn = document.getElementById("nav-bar-mode-btn");
-const ModeBtnTextCon = document.getElementsByClassName("bx-moon");
-const DarkBtn = document.getElementById("nav-bar-menu-btn");
+/* theme bnt collections */
+const ThemeBtn = document.getElementById("theme-btn");
+const ThemeBtnIcon = document.getElementById("theme-icon");
 
-ModeBtn.addEventListener("click", () => {
-  document.documentElement.classList.toggle("dark");
+/* nev links collections */
+const DropBarBtn = document.getElementById("drop-bar-btn");
+const NavLinks = document.getElementById("nav-links");
+const CloseDropBarBtn = document.getElementById("close-drop-bar-btn");
+
+ThemeBtn.addEventListener("click", () => {
+  document.documentElement.classList.toggle("dark-colors");
+
+  const isDark = document.documentElement.classList.contains("dark-colors");
+
+  if (isDark) {
+    ThemeBtnIcon.classList.remove("bx-moon");
+    ThemeBtnIcon.classList.add("bx-sun");
+  } else {
+    ThemeBtnIcon.classList.remove("bx-sun");
+    ThemeBtnIcon.classList.add("bx-moon");
+  }
+});
+
+DropBarBtn.addEventListener("click", () => {
+  NavLinks.style.display = "flex";
+});
+
+CloseDropBarBtn.addEventListener("click", () => {
+  NavLinks.style.display = "none";
 });
